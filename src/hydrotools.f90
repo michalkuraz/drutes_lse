e08b384855a8b443144d3aef356ed25822335732
Nour <dalin@fzp.czu.cz>
2026-09-11 11:02:14 +0200

    activate allocations for meteodata

--- src/hydrotools.f90
+++ src/hydrotools.f90
@@ -1,3 +1,4 @@
+  
 module hydrotools
   use typy
   use globals
@@ -14,15 +15,15 @@ contains
     character(len=*), intent(in) :: filename
 
     integer :: fileid, ios
-    integer(kind=ikind) :: i
-    real(kind=rkind) :: node_id, element_id
+    integer :: i
+    real :: node_id, element_id
 
-    open(newunit=fileid, file=filename, status='old', action='read', iostat=ios)
-    if (ios /= 0) error stop 'Error: could not open mesh file.'
+    open(unit=fileid, file=filename, status='old', action='read', iostat=ios)
+    if (ios /= 0) stop 'Error: could not open mesh file.'
 
     call comment(fileid)
     read(fileid, *, iostat=ios) nodes%kolik
-    if (ios /= 0) error stop 'Error: failed to read number of nodes.'
+    if (ios /= 0) stop 'Error: failed to read number of nodes.'
 
     allocate(nodes%data(nodes%kolik, 2))
     allocate(nodes%altitude(nodes%kolik))
@@ -30,30 +31,31 @@ contains
     do i = 1, nodes%kolik
       call comment(fileid)
       read(fileid, *, iostat=ios) node_id, nodes%data(i,1), nodes%data(i,2), nodes%altitude(i)
-      if (ios /= 0) error stop 'Error: failed to read node data.'
+      if (ios /= 0) stop 'Error: failed to read node data.'
     end do
 
     call comment(fileid)
     read(fileid, *, iostat=ios) elements%kolik
-    if (ios /= 0) error stop 'Error: failed to read number of elements.'
+    if (ios /= 0) stop 'Error: failed to read number of elements.'
 
     call mesh_allocater()
 
     do i = 1, elements%kolik
       call comment(fileid)
       read(fileid, *, iostat=ios) element_id, elements%data(i,1), elements%data(i,2), elements%data(i,3)
-      if (ios /= 0) error stop 'Error: failed to read element data.'
+      if (ios /= 0) stop 'Error: failed to read element data.'
     end do
 
     close(fileid)
   end subroutine read_mesh
 
 
+
   !==============================================================
   ! ALLOCATE MESH ARRAYS
   !==============================================================
   subroutine mesh_allocater()
-    integer(kind=ikind) :: el
+    integer :: el
 
     if (.not. allocated(elements%data))       allocate(elements%data(elements%kolik, 3))
     if (.not. allocated(elements%area))       allocate(elements%area(elements%kolik))
@@ -66,33 +68,33 @@ contains
     if (.not. allocated(elements%downstream)) allocate(elements%downstream(elements%kolik))
     if (.not. allocated(elements%hydrobal))   allocate(elements%hydrobal(elements%kolik))
 
-    elements%data       = 0_ikind
-    elements%area       = 0.0_rkind
-    elements%material   = 0_ikind
-    elements%avgalt     = 0.0_rkind
-    elements%slope      = 0.0_rkind
-    elements%overflow   = 0.0_rkind
-    elements%neighbours = 0_ikind
+    elements%data       = 0
+    elements%area       = 0.0
+    elements%material   = 0
+    elements%avgalt     = 0.0
+    elements%slope      = 0.0
+    elements%overflow   = 0.0
+    elements%neighbours = 0
 
     do el = 1, elements%kolik
-      elements%upstream(el)%els    = 0_ikind
-      elements%upstream(el)%slopes = 0.0_rkind
-      elements%upstream(el)%widths = 0.0_rkind
+      elements%upstream(el)%els    = 0
+      elements%upstream(el)%slopes = 0.0
+      elements%upstream(el)%widths = 0.0
 
-      elements%downstream(el)%els    = 0_ikind
-      elements%downstream(el)%slopes = 0.0_rkind
-      elements%downstream(el)%widths = 0.0_rkind
+      elements%downstream(el)%els    = 0
+      elements%downstream(el)%slopes = 0.0
+      elements%downstream(el)%widths = 0.0
 
-      elements%hydrobal(el)%inflow  = 0.0_rkind
-      elements%hydrobal(el)%outflow = 0.0_rkind
-      elements%hydrobal(el)%ET      = 0.0_rkind
-      elements%hydrobal(el)%q1      = 0.0_rkind
-      elements%hydrobal(el)%q2      = 0.0_rkind
-      elements%hydrobal(el)%q3      = 0.0_rkind
-      elements%hydrobal(el)%pc      = 0.0_rkind
-      elements%hydrobal(el)%bf      = 0.0_rkind
-      elements%hydrobal(el)%deltas  = 0.0_rkind
-      elements%hydrobal(el)%storage = 0.0_rkind
+      elements%hydrobal(el)%inflow  = 0.0
+      elements%hydrobal(el)%outflow = 0.0
+      elements%hydrobal(el)%ET      = 0.0
+      elements%hydrobal(el)%q1      = 0.0
+      elements%hydrobal(el)%q2      = 0.0
+      elements%hydrobal(el)%q3      = 0.0
+      elements%hydrobal(el)%pc      = 0.0
+      elements%hydrobal(el)%bf      = 0.0
+      elements%hydrobal(el)%deltas  = 0.0
+      elements%hydrobal(el)%storage = 0.0
     end do
   end subroutine mesh_allocater
 
@@ -108,11 +110,11 @@ contains
 
   if (.not. allocated(meteodata)) allocate(meteodata(1))
 
-  open(newunit=unit, file=filename, status="old", action="read", iostat=ios)
+  open(unit=unit, file=filename, status='old', action='read', iostat=ios)
 
   if (ios /= 0) then
     print *, "ERROR: cannot open meteorological CSV file: ", trim(filename)
-    error stop
+    stop
   end if
 
   ! Skip header line
@@ -136,7 +138,7 @@ contains
 
   if (meteodata(1)%time%pos < 1) then
     print *, "ERROR: meteorological file is empty."
-    error stop
+    stop
   end if
 
   print *, "Meteo records read = ", meteodata(1)%time%pos
@@ -147,16 +149,16 @@ end subroutine read_meteodata_csv
   ! INITIALIZE HYDROLOGY
   !==============================================================
   subroutine init_hydro()
-    integer(kind=ikind) :: i, t, d
+    integer :: i, t, d
 
-    !real(kind=rkind), dimension(10) :: pday
-    !real(kind=rkind), dimension(10) :: uzday
-    !real(kind=rkind), dimension(10) :: tmaxday
-    !real(kind=rkind), dimension(10) :: tminday
-    !real(kind=rkind), dimension(10) :: tmeanday
-    !real(kind=rkind), dimension(10) :: rhmaxday
-    !real(kind=rkind), dimension(10) :: rhminday
-    !real(kind=rkind), dimension(10) :: soilday
+    !real, dimension(10) :: pday
+    !real, dimension(10) :: uzday
+    !real, dimension(10) :: tmaxday
+    !real, dimension(10) :: tminday
+    !real, dimension(10) :: tmeanday
+    !real, dimension(10) :: rhmaxday
+    !real, dimension(10) :: rhminday
+    !real, dimension(10) :: soilday
 
     allocate(precip(elements%kolik, n_steps))
     allocate(qinter(elements%kolik, n_steps))
@@ -168,13 +170,13 @@ end subroutine read_meteodata_csv
     allocate(Ksat_gw(elements%kolik))
     allocate(G(elements%kolik))
 
-    !allocate(Tmax(elements%kolik, n_steps))
-    !allocate(Tmin(elements%kolik, n_steps))
-    !allocate(Tmean(elements%kolik, n_steps))
-    !allocate(RHmax(elements%kolik, n_steps))
-    !allocate(RHmin(elements%kolik, n_steps))
-    !allocate(uz(elements%kolik, n_steps))
-    !allocate(soilcontent(elements%kolik, n_steps))
+    allocate(Tmax(elements%kolik, n_steps))
+    allocate(Tmin(elements%kolik, n_steps))
+    allocate(Tmean(elements%kolik, n_steps))
+    allocate(RHmax(elements%kolik, n_steps))
+    allocate(RHmin(elements%kolik, n_steps))
+    allocate(uz(elements%kolik, n_steps))
+    allocate(soilcontent(elements%kolik, n_steps))
 
     allocate(Pm(elements%kolik, n_steps))
     allocate(E_m(elements%kolik, n_steps))
@@ -209,85 +211,85 @@ end subroutine read_meteodata_csv
     allocate(capacity(elements%kolik))
     allocate(outlet_Q(n_steps))
 
-    precip      = 0.0_rkind
-    qinter      = 0.0_rkind
-    qout        = 0.0_rkind
-    conduct     = 0.0_rkind
-    G           = 0.0_rkind
+    precip      = 0.0
+    qinter      = 0.0
+    qout        = 0.0
+    conduct     = 0.0
+    G           = 0.0
 
-    !Tmax        = 0.0_rkind
-    !Tmin        = 0.0_rkind
-    !Tmean       = 0.0_rkind
-    !RHmax       = 0.0_rkind
-    !RHmin       = 0.0_rkind
-    !uz          = 0.0_rkind
-    !soilcontent = 0.0_rkind
+    Tmax        = 0.0
+    Tmin        = 0.0
+    Tmean       = 0.0
+    RHmax       = 0.0
+    RHmin       = 0.0
+    uz          = 0.0
+    soilcontent = 0.0
 
-    Pm = 0.0_rkind
-    E_m = 0.0_rkind
-    If_m = 0.0_rkind
-    q1 = 0.0_rkind
-    q2 = 0.0_rkind
-    q3 = 0.0_rkind
-    pc = 0.0_rkind
-    bf = 0.0_rkind
+    Pm = 0.0
+    E_m = 0.0
+    If_m = 0.0
+    q1 = 0.0
+    q2 = 0.0
+    q3 = 0.0
+    pc = 0.0
+    bf = 0.0
 
-    Ssurf = 0.0_rkind
-    Ssub  = 0.0_rkind
-    Sgw   = 0.0_rkind
+    Ssurf = 0.0
+    Ssub  = 0.0
+    Sgw   = 0.0
 
-    dSsurf = 0.0_rkind
-    dSsub  = 0.0_rkind
-    dSgw   = 0.0_rkind
+    dSsurf = 0.0
+    dSsub  = 0.0
+    dSgw   = 0.0
 
-    Ssurf_hist = 0.0_rkind
-    Ssub_hist  = 0.0_rkind
-    Sgw_hist   = 0.0_rkind
+    Ssurf_hist = 0.0
+    Ssub_hist  = 0.0
+    Sgw_hist   = 0.0
 
-    Qin_result      = 0.0_rkind
-    Qout_result     = 0.0_rkind
-    Overflow_result = 0.0_rkind
-    Storage_result  = 0.0_rkind
-    deltas       = 0.0_rkind
-    total_deltaS = 0.0_rkind
-    outlet_Q_m3s    = 0.0_rkind
+    Qin_result      = 0.0
+    Qout_result     = 0.0
+    Overflow_result = 0.0
+    Storage_result  = 0.0
+    deltas       = 0.0
+    total_deltaS = 0.0
+    outlet_Q_m3s    = 0.0
 
-    storage  = 0.0_rkind
-    capacity = 8.0_rkind
-    outlet_Q = 0.0_rkind
+    storage  = 0.0
+    capacity = 8.0
+    outlet_Q = 0.0
 
     !if (n_steps < 10) stop "Need at least 10 time steps for the hardcoded forcing."
 
-    !pday = [0.0_rkind, 0.0_rkind, 17.0_rkind, 12.0_rkind, 9.0_rkind, &
-    !        7.0_rkind, 40.0_rkind, 0.0_rkind, 0.0_rkind, 3.0_rkind]
+    !pday = [0.0, 0.0, 17.0, 12.0, 9.0, &
+    !        7.0, 40.0, 0.0, 0.0, 3.0]
 
-    !uzday = [4.38_rkind, 3.57_rkind, 4.026_rkind, 3.097_rkind, 4.14_rkind, &
-    !         3.13_rkind, 3.92_rkind, 3.19_rkind, 3.98_rkind, 3.34_rkind]
+    !uzday = [4.38, 3.57, 4.026, 3.097, 4.14, &
+    !         3.13, 3.92, 3.19, 3.98, 3.34]
 
-    !tmaxday = [19.1_rkind, 15.3_rkind, 12.8_rkind, 11.8_rkind, 10.5_rkind, &
-    !           15.2_rkind, 11.6_rkind, 14.6_rkind, 17.2_rkind, 16.4_rkind]
+    !tmaxday = [19.1, 15.3, 12.8, 11.8, 10.5, &
+    !           15.2, 11.6, 14.6, 17.2, 16.4]
 
-    !tminday = [5.4_rkind, 6.8_rkind, 8.8_rkind, 7.6_rkind, 8.4_rkind, &
-    !           8.3_rkind, 8.8_rkind, 6.2_rkind, 4.8_rkind, 6.2_rkind]
+    !tminday = [5.4, 6.8, 8.8, 7.6, 8.4, &
+    !           8.3, 8.8, 6.2, 4.8, 6.2]
 
-    !tmeanday = [12.25_rkind, 11.05_rkind, 10.8_rkind, 9.7_rkind, 9.45_rkind, &
-    !            11.75_rkind, 10.2_rkind, 10.4_rkind, 11.0_rkind, 9.7_rkind]
+    !tmeanday = [12.25, 11.05, 10.8, 9.7, 9.45, &
+    !            11.75, 10.2, 10.4, 11.0, 9.7]
 
-    !rhmaxday = [84.0_rkind, 85.0_rkind, 76.0_rkind, 87.0_rkind, 92.0_rkind, &
-               !94.0_rkind, 97.0_rkind, 92.0_rkind, 93.0_rkind, 97.0_rkind]
+    !rhmaxday = [84.0, 85.0, 76.0, 87.0, 92.0, &
+               !94.0, 97.0, 92.0, 93.0, 97.0]
 
-    !rhminday = [56.0_rkind, 64.0_rkind, 64.0_rkind, 77.0_rkind, 77.0_rkind, &
-    !            76.0_rkind, 74.0_rkind, 59.0_rkind, 62.0_rkind, 61.0_rkind]
+    !rhminday = [56.0, 64.0, 64.0, 77.0, 77.0, &
+    !            76.0, 74.0, 59.0, 62.0, 61.0]
 
-    !soilday = [0.32_rkind, 0.34_rkind, 0.33_rkind, 0.30_rkind, 0.27_rkind, &
-               !0.24_rkind, 0.22_rkind, 0.23_rkind, 0.26_rkind, 0.29_rkind]
+    !soilday = [0.32, 0.34, 0.33, 0.30, 0.27, &
+               !0.24, 0.22, 0.23, 0.26, 0.29]
 
     !do i = 1, elements%kolik
      ! do t = 1, n_steps
         !d = int((t - 1) / 24) + 1
        ! if (d > 10) d = 10
 
-        !precip(i,t)      = pday(d) / 24.0_rkind
+        !precip(i,t)      = pday(d) / 24.0
        ! uz(i,t)          = uzday(d)
        ! Tmax(i,t)        = tmaxday(d)
         !Tmin(i,t)        = tminday(d)
@@ -308,54 +310,54 @@ end subroutine read_meteodata_csv
           d = meteodata(1)%time%pos
           end if
 
-    precip(i,t)      = meteodata(1)%rainfall%data(d) / 24.0_rkind
+    precip(i,t)      = meteodata(1)%rainfall%data(d) / 24.0
     uz(i,t)          = meteodata(1)%wind%data(d)
     Tmax(i,t)        = meteodata(1)%Tmax%data(d)
     Tmin(i,t)        = meteodata(1)%Tmin%data(d)
-    Tmean(i,t)       = 0.5_rkind * (Tmax(i,t) + Tmin(i,t))
+    Tmean(i,t)       = 0.5 * (Tmax(i,t) + Tmin(i,t))
     RHmax(i,t)       = meteodata(1)%RHmax%data(d)
     RHmin(i,t)       = meteodata(1)%RHmin%data(d)
     soilcontent(i,t) = meteodata(1)%soilcontent%data(d)
     end do
       end do
 
-    conduct   = 0.00002_rkind
-    Ksat_surf = 1.0e-6_rkind
-    Ksat_sub  = 5.0e-7_rkind
-    Ksat_gw   = 1.0e-6_rkind
+    conduct   = 0.00002
+    Ksat_surf = 1.0e-6
+    Ksat_sub  = 5.0e-7
+    Ksat_gw   = 1.0e-6
 
-    ksurf_exp = 3.0_rkind
-    ksub_exp  = 2.0_rkind
+    ksurf_exp = 3.0
+    ksub_exp  = 2.0
 
     CN         = 98
-    z          = 235.0_rkind
+    z          = 235.0
     Julian_day = 172
-    phi        = 0.614_rkind
-    as         = 0.25_rkind
-    bs         = 0.5_rkind
-    alpha      = 0.23_rkind
-    sigma      = 4.903e-5_rkind
-    gsc        = 0.0820_rkind
-    ccrop      = 0.95_rkind
+    phi        = 0.614
+    as         = 0.25
+    bs         = 0.5
+    alpha      = 0.23
+    sigma      = 4.903e-5
+    gsc        = 0.0820
+    ccrop      = 0.95
 
-    cn_slope_coeff      = 0.15_rkind
-    storage_slope_coeff = 4.0_rkind
-    qgw_slope_coeff     = 0.5_rkind
-    min_edge_width      = 1.0e-6_rkind
+    cn_slope_coeff      = 0.15
+    storage_slope_coeff = 4.0
+    qgw_slope_coeff     = 0.5
+    min_edge_width      = 1.0e-6
 
-    theta_r = 0.08_rkind
-    theta_s = 0.42_rkind
+    theta_r = 0.08
+    theta_s = 0.42
 
-    Beta1 = 0.0_rkind
-    Beta2 = 0.05_rkind
-    Beta3 = 1.0_rkind
-    Beta4 = 0.03_rkind
-    Beta5 = 0.02_rkind
+    Beta1 = 0.0
+    Beta2 = 0.05
+    Beta3 = 1.0
+    Beta4 = 0.03
+    Beta5 = 0.02
 
-    z1 = 0.0_rkind
-    z2 = 1.0_rkind
+    z1 = 0.0
+    z2 = 1.0
 
-    infil_slope_coeff = 8.0_rkind
+    infil_slope_coeff = 8.0
 
   end subroutine init_hydro
 
